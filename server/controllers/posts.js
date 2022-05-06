dfacec538d420d2e8977ec5f6d5c10327fa0a8ef
Jesus Velasco <jesusvelascov@gmail.com>
2022-05-06 00:49:43 -0400

    Feat: Agregar metodo getLatestPosts y update de los otros

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,28 +1,22 @@
 import express from 'express';
 
-
-
 import PostMessage from '../models/Cars.js';
 
 
 const router = express.Router();
 
 export const getPosts = async (req, res) => {
+  
   const { page } = req.query;
 
-
   try {
+
     const LIMIT = 4;
     const startIndex = (Number(page) - 1) * LIMIT;
     const total = await PostMessage.countDocuments({});
-
-    //console.log( Math.ceil(total / LIMIT))
-
-    const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
-    //console.log(posts);
-
-
+    const posts = await PostMessage.find().select('-othersImg').sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
     res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
+
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -30,95 +24,74 @@ export const getPosts = async (req, res) => {
 
 
 export const getPostsByCategory = async (req, res) => {
-  const filter = req.query
 
-  const { sort } = req.query
-
+  const SORT_CASES = {
+    'id' : '-_id',
+    'reciente' : '-createdAt',
+    'descendente': '-price',
+    'ascendente': 'price'
+  }
+  console.log(req.query)
+  const arrayQuery = req.query;
+  const { sort } = req.query || 'id';
   const { page } = req.query
-
-  var arrayQuery = filter;
-
-  delete arrayQuery.page
-  delete arrayQuery.sort
-
+  delete arrayQuery.page;
+  delete arrayQuery.sort;
 
   try {
 
     const LIMIT = 4;
     const startIndex = (Number(page) - 1) * LIMIT;
-
-
+    let finalQuery = {...arrayQuery};
 
     if (arrayQuery.minPrice && arrayQuery.maxPrice !== '') {
 
-      const minPrice = Number(arrayQuery.minPrice)
-      const maxPrice = Number(arrayQuery.maxPrice)
-      delete arrayQuery.minPrice
-      delete arrayQuery.maxPrice
+      const minPrice = Number(finalQuery.minPrice);
+      const maxPrice = Number(finalQuery.maxPrice);
+      delete finalQuery.minPrice;
+      delete finalQuery.maxPrice;
+      finalQuery = { $and: [finalQuery, { $and: [{ price: { $gte: minPrice } }, { price: { $lte: maxPrice } }] }] };   
 
-
-
-      const posts = await PostMessage.find({ $and: [arrayQuery, { $and: [{ price: { $gte: minPrice } }, { price: { $lte: maxPrice } }] }] }).limit(LIMIT).skip(startIndex);
-
-
-      const total = await PostMessage.find(arrayQuery);
-
-
-
-
-      res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total.length / LIMIT), filters: arrayQuery });
-
-    } else {
-      const posts = await PostMessage.find(arrayQuery).limit(LIMIT).skip(startIndex);
-
-      const total = await PostMessage.find(arrayQuery);
-      //console.log(posts)
-
-      if (sort === 'reciente') {
-
-        posts.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
-
-      }
-
-      if (sort === 'descendente') {
-
-        posts.sort((a, b) => b.price - a.price);
-
-      }
-
-      if (sort === 'ascendente') {
-
-        posts.sort((a, b) => a.price - b.price);
-
-      }
-
-      res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total.length / LIMIT), filters: arrayQuery });
-
-    }
-
-
-
-
+    } 
+    
+    const total = await PostMessage.countDocuments(finalQuery);
+    const posts = await PostMessage.find(finalQuery).select('-othersImg').limit(LIMIT).skip(startIndex).sort(SORT_CASES[sort]);
+    //const posts = await PostMessage.find(finalQuery).select('-othersImg -mainImg').limit(LIMIT).skip(startIndex).sort(SORT_CASES[sort]);
+    res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT), filters: arrayQuery });
 
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
-
 
 }
 
 
 export const getPost = async (req, res) => {
-  const { id } = req.params;
 
+  const { id } = req.params;
 
   try {
     const post = await PostMessage.findById(id);
-
     res.status(200).json(post);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
+
 }
+
+
+export const getLatestPosts = async (req, res) => {
+  
+  try {
+
+    const LIMIT = 5;
+    const posts = await PostMessage.find().select('-othersImg').sort('-createdAt').limit(LIMIT);
+    res.status(200).json({ posts });
+    
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
 
 export default router;
