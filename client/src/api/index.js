5eb99dcad54580dfadd8ff9c7257783592a1bba0
mrodriguez <manuelrodriguezmog@gmail.com>
2022-05-17 15:57:20 -0400

    Probando vercel

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 const url = 'http://localhost:5000/posts/catalogo';
 
 const API = axios.create({ baseURL: 'https://tu-carro-propio.vercel.app' });
+//const API = axios.create({ baseURL: 'http://localhost:5000' });
+
 
 export const fetchPosts = (page) => API.get(`/posts/catalogo/search?page=${page}`)
 
