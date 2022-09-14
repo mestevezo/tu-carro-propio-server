51f182b6654b2aeec39de257df7d54fbfbdd3821
Miguelangel <mestevezo23@gmail.com>
2022-09-13 21:16:56 -0400

    Fix: Agrega menú contextual a los items del catálogo

--- client/src/pages/Catálogo/Posts/Post/Post.js
+++ client/src/pages/Catálogo/Posts/Post/Post.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { ButtonBase } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { ButtonBase } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-
-import useStyles from './styles';
+import useStyles from "./styles";
 
 const Post = ({ post }) => {
-
   const classes = useStyles();
   const navigate = useNavigate();
 
@@ -18,28 +17,49 @@ const Post = ({ post }) => {
   };
 
   return (
-    <Card className={classes.card}>
-      <ButtonBase
-        component="span"
-        className={classes.cardAction}
-        onClick={openPost}
+    <>
+      <Link
+        to={{ pathname: `/catalogo/${post._id}` }}
+        style={{ textDecoration: "none" }}
       >
-        <CardMedia className={classes.media} image={post.mainImg || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
-        <div className={classes.overlay}>
-          <h2>{post.price === 0 ? "Precio a consultar" : post.price + ' USD'}</h2>
-        </div>
-        <br></br>
-        <h2 className={classes.title} variant="h5" component="h2">{post.brand + ' ' + post.model + ' ' + post.version + ' ' + post.year}</h2>
-        <div className={classes.details}>
-          <h3 variant="body2" color="textSecondary" component="h2">{post.km + ' Km'}</h3>
-        </div>
-        <CardContent>
-          <p>{post.transmission + ' / ' + post.fuel}</p>
-        </CardContent>
-      </ButtonBase>
-    </Card>
+        <Card className={classes.card}>
+          <ButtonBase component="span" className={classes.cardAction}>
+            <CardMedia
+              className={classes.media}
+              image={
+                post.mainImg ||
+                "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+              }
+              title={post.title}
+            />
+            <div className={classes.overlay}>
+              <h2>
+                {post.price === 0 ? "Precio a consultar" : post.price + " USD"}
+              </h2>
+            </div>
+            <br></br>
+            <h2 className={classes.title} variant="h5" component="h2">
+              {post.brand +
+                " " +
+                post.model +
+                " " +
+                post.version +
+                " " +
+                post.year}
+            </h2>
+            <div className={classes.details}>
+              <h3 variant="body2" color="textSecondary" component="h2">
+                {post.km + " Km"}
+              </h3>
+            </div>
+            <CardContent>
+              <p>{post.transmission + " / " + post.fuel}</p>
+            </CardContent>
+          </ButtonBase>
+        </Card>
+      </Link>
+    </>
   );
-
 };
 
 export default Post;
